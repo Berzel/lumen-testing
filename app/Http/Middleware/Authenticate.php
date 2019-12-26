f42b27eee4a7301a1ac9f92fc7cc226125d804cf
Berzel <berzelbtumbude@gmail.com>
2019-12-26 17:46:49 +0200

    Used an abort instead of throwing an error

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Utillities\HttpStatus;
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
 
@@ -36,7 +37,7 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            abort(HttpStatus::UNAUTHORIZED, 'Not Authenticated');
         }
 
         return $next($request);
